83b5d43b69628bd4d6438c598312a84934c4ce82
eminom <hai.bai.cn@gmail.com>
2015-04-18 17:45:22 +0800

    fixed

--- scripts/2.py
+++ scripts/2.py
@@ -1,6 +1,22 @@
-from FixTest import Fixed16
-a = Fixed16(20)
-b = 30.0
-a/=b
-print("/= is done")
-print(a)
+
+if __name__ == "__main__":
+    from FixTest import Fixed16
+    a = Fixed16(20)
+    a = Fixed16(20) + 20.0
+    a = 20.0 / Fixed16(30)
+    a = Fixed16(100) * Fixed16(20.3)
+
+    print("")
+    print("Test inplace operator:")
+    a/=20.0
+    a/=a
+    try:
+        a = Fixed16(0)
+        print("a = ", a)
+        x = 30.0
+        x /= a
+        print(x)
+    except:
+        print("Error happed")
+        raise
+
